fc52f797dec18fd4f7caf459354adeaebfc609b2
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-08-08 01:56:06 +0200

    Add blurry images as placeholder

--- osti-website/components/TestLink.jsx
+++ osti-website/components/TestLink.jsx
@@ -3,7 +3,16 @@ import Image from "next/image";
 const TestLinkImage = ({ width, height, url, src }) => {
 	return (
 		<a href={url} target="_blank" rel="noreferrer">
-			<Image src={src} width={width} height={height} alt={url}></Image>
+			<Image
+				placeholder="blur"
+				src={src}
+				layout="responsive"
+				width={width}
+				height={height}
+				alt={url}
+				loading="lazy"
+				blurDataURL={`/_next/image?url=${src}&w=16&q=1`}
+			></Image>
 		</a>
 	);
 };
